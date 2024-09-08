f4ac27fd072f5d88f2359a2bfc8efd126e3161ad
Valeria Koroleva <koroleva.v23@wildberries.work>
2024-09-08 16:39:37 +0300

    models

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
+mongoose.connect('mongodb://localhost:27017/mestodb');
 const { PORT = 3000, BASE_PATH = 'none' } = process.env;
 
 const app = express();
